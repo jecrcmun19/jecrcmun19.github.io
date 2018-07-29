7d1b3c7b77d57aa859098f19e63fb9b9094a5562
aditya81070 <aditya81070@gmail.com>
2018-07-29 23:28:30 +0530

    initial commit

---
+++ scripts/gtag.js
@@ -0,0 +1,11 @@
+// Global site tag (gtag.js) - Google Analytics
+< script async src = "https://www.googletagmanager.com/gtag/js?id=UA-114896644-1" > < /script>
+window.dataLayer = window.dataLayer || [];
+
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag('js', new Date());
+
+gtag('config', 'UA-114896644-1'); <
+/script >
