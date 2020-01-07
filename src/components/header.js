ef9af09209e4b8f2f26542ba3438feef4a864187
Keshav <ksvkabra@gmail.com>
2020-01-08 01:15:56 +0530

    Added Mobile drawer

--- src/components/header.js
+++ src/components/header.js
@@ -10,7 +10,9 @@ import {
   Typography,
   ListItem,
   List,
+  useMediaQuery,
 } from '@material-ui/core'
+import MobileViewDrawer from './mobileDrawer'
 
 const transparentTheme = createMuiTheme({
   palette: {
@@ -64,6 +66,7 @@ export default function Header(props) {
   const classes = useStyles()
   const [scrollY, setScrollY] = useState(0)
 
+  // header scroll control
   window.onscroll = logScroll
 
   function logScroll(e) {
@@ -71,56 +74,82 @@ export default function Header(props) {
     setScrollY(scrollY)
   }
 
-  return (
-    <ThemeProvider theme={scrollY < 110 ? transparentTheme : blackTheme}>
-      <AppBar className={scrollY < 110 ? classes.header : classes.onScroll}>
-        <Toolbar>
-          <Grid container justify='flex-start' lg={1}>
-            <Grid item lg={1} md={2} sm={1}>
-              <Avatar className={classes.black}>A</Avatar>
+  // mobile-view handling
+  const matches = useMediaQuery('(min-width:800px)')
+  console.log(matches)
+  if (matches)
+    return (
+      <ThemeProvider theme={scrollY < 110 ? transparentTheme : blackTheme}>
+        <AppBar className={scrollY < 110 ? classes.header : classes.onScroll}>
+          <Toolbar>
+            <Grid container justify='flex-start'>
+              <Grid item lg={1} md={2} sm={1}>
+                <Avatar className={classes.black}>A</Avatar>
+              </Grid>
             </Grid>
-          </Grid>
-          <Grid container justify='flex-end' lg={8}>
-            <List className={classes.horizontalList}>
-              <ListItem>
-                <Typography className={classes.headerItems} variant='subtitle2'>
-                  HOME
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.headerItems} variant='subtitle2'>
-                  ABOUT
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.headerItems} variant='subtitle2'>
-                  COMITTEES
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.headerItems} variant='subtitle2'>
-                  REGISTRATIONS
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.headerItems} variant='subtitle2'>
-                  BLOGS
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.headerItems} variant='subtitle2'>
-                  GALLERY
-                </Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.headerItems} variant='subtitle2'>
-                  CONTACT
-                </Typography>
-              </ListItem>
-            </List>
-          </Grid>
-        </Toolbar>
-      </AppBar>
-    </ThemeProvider>
-  )
+            <Grid container justify='flex-end'>
+              <List className={classes.horizontalList}>
+                <ListItem>
+                  <Typography
+                    className={classes.headerItems}
+                    variant='subtitle2'
+                  >
+                    HOME
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography
+                    className={classes.headerItems}
+                    variant='subtitle2'
+                  >
+                    ABOUT
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography
+                    className={classes.headerItems}
+                    variant='subtitle2'
+                  >
+                    COMITTEES
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography
+                    className={classes.headerItems}
+                    variant='subtitle2'
+                  >
+                    REGISTRATIONS
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography
+                    className={classes.headerItems}
+                    variant='subtitle2'
+                  >
+                    BLOGS
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography
+                    className={classes.headerItems}
+                    variant='subtitle2'
+                  >
+                    GALLERY
+                  </Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography
+                    className={classes.headerItems}
+                    variant='subtitle2'
+                  >
+                    CONTACT
+                  </Typography>
+                </ListItem>
+              </List>
+            </Grid>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
+    )
+  else return <MobileViewDrawer />
 }
