8673ee0b94deead4cf4fced42ad0ccde0f561379
Aditya Agarwal <aditya81070@gmail.com>
2020-01-12 11:14:02 +0530

    feat: add image transformer and fix banner image

--- src/components/banner.js
+++ src/components/banner.js
@@ -1,25 +1,19 @@
 import React from 'react'
 import { makeStyles, Typography } from '@material-ui/core'
-
-const styles = {
-  backgroundImage: `linear-gradient(rgba(41, 24, 2, 0.75), rgba(41, 24, 2, 0.75)), url(images/bg.png)`,
-  height: '100vh',
-  backgroundRepeat: 'no-repeat',
-  backgroundSize: 'cover',
-  backgroundPosition: 'center center',
-  backgroundAttachment: 'fixed',
-}
+import { graphql, useStaticQuery } from 'gatsby'
+import BackgroundImage from 'gatsby-background-image'
 
 const useStyles = makeStyles(theme => ({
-  content: {
-    display: 'table-cell',
-    verticalAlign: 'middle',
-    textAlign: 'center',
-  },
   header: {
-    display: 'table',
     width: '100%',
-    zIndex: 1200,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center center',
+    backgroundAttachment: 'fixed',
   },
   munTextProperty: {
     color: '#FF8E01',
@@ -39,21 +33,32 @@ const useStyles = makeStyles(theme => ({
 
 function Banner(props) {
   const classes = useStyles()
+  const { image } = useStaticQuery(graphql`
+    query {
+      image: file(relativePath: { eq: "bg.png" }) {
+        sharp: childImageSharp {
+          fluid {
+            ...GatsbyImageSharpFluid_withWebp
+          }
+        }
+      }
+    }
+  `)
+  const backgrounds = [
+    `linear-gradient(rgba(41, 24, 2, 0.75), rgba(41, 24, 2, 0.75))`,
+    image.sharp.fluid,
+  ]
   return (
-    <div>
-      <div style={styles} className={classes.header}>
-        <div className={classes.content}>
-          <div>
-            <Typography className={classes.munTextProperty}>
-              JECRC MUN 2020
-            </Typography>
-            <Typography className={classes.diplomacyTextProperty}>
-              Deplomacy At It’s Zenith
-            </Typography>
-          </div>
-        </div>
+    <BackgroundImage className={classes.header} fluid={backgrounds}>
+      <div className='text-center'>
+        <Typography className={classes.munTextProperty}>
+          JECRC MUN 2020
+        </Typography>
+        <Typography className={classes.diplomacyTextProperty}>
+          Deplomacy At It’s Zenith
+        </Typography>
       </div>
-    </div>
+    </BackgroundImage>
   )
 }
 
