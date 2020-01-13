95fa92445ce353ceb345c342d3eb9cb104789d5f
Keshav <ksvkabra@gmail.com>
2020-01-13 14:54:01 +0530

    WIP: Country Flag styling completed

--- src/pages/country-matrix.js
+++ src/pages/country-matrix.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import Wrapper from '../components/wrapper'
 import CardContent from '@material-ui/core/CardContent'
-import Typography from '@material-ui/core/Typography'
 import Card from '@material-ui/core/Card'
 import makeStyles from '@material-ui/styles/makeStyles'
 import Grid from '@material-ui/core/Grid'
@@ -22,6 +21,7 @@ const useStyles = makeStyles(theme => ({
   root: {
     paddingTop: 10,
     paddingBottom: 10,
+    background: '#D90845',
   },
   cardContent: {
     width: '100%',
@@ -49,6 +49,26 @@ const useStyles = makeStyles(theme => ({
     lineHeight: 1,
     fontSize: 20,
   },
+  backgroundCardProperty: {
+    // margin: '50px',
+  },
+  bgCardContentProperty: {
+    padding: 0,
+    // margin: '40px',
+  },
+  countryMatrixBackground: {
+    background: 'url(images/committees-card.png)',
+    backgroundPosition: 'center center',
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: 'cover',
+    minHeight: '300px',
+    padding: '6%',
+  },
+  tabsTextProperty: {
+    fontSize: '24px',
+    fontWeight: '500',
+    color: '#FFF',
+  },
 }))
 
 function CountryMatrix(props) {
@@ -63,48 +83,58 @@ function CountryMatrix(props) {
   return (
     <Wrapper>
       <div className={bannerClasses}></div>
-      <Grid container justify='center' className={classes.root}>
-        <Tabs
-          value={value}
-          variant='scrollable'
-          onChange={handleChange}
-          indicatorColor='primary'
-          textColor='primary'
-          // centered
-          scrollButtons='desktop'
-        >
-          <Tab label='UNSC' />
-          <Tab label='UNHRC' />
-          <Tab label='DISEC' />
-          <Tab label='LokSabha' />
-          <Tab label='CSW' />
-          <Tab label='IMF' />
-          <Tab label='CSTD' />
-          <Tab label='NSG' />
-          <Tab label='SPECPOL' />
-          <Tab label='WHA' />
-        </Tabs>
-      </Grid>
-      <div className={classes.cardStyle}>
-        <Grid container justify='center' alignContent='space-around'>
-          {Committe[value].map((text, index) => (
-            <Grid
-              item
-              className={classes.listItemStyle}
-              xl={2}
-              md={4}
-              lg={3}
-              sm={6}
-              xs={12}
-            >
-              <Card className={classes.cardProperty}>
-                <CardContent className={classes.textProperty}>
-                  {text}
-                </CardContent>
-              </Card>
-            </Grid>
-          ))}
-        </Grid>
+      <div className={classes.countryMatrixBackground}>
+        <div>
+          <Card className={classes.backgroundCardProperty} raised={true}>
+            <CardContent className={classes.bgCardContentProperty}>
+              <Grid container justify='center' className={classes.root}>
+                <Tabs
+                  value={value}
+                  variant='scrollable'
+                  onChange={handleChange}
+                  indicatorColor='#FFFF8C'
+                  centered
+                  scrollButtons='desktop'
+                >
+                  <Tab className={classes.tabsTextProperty} label='UNSC' />
+                  <Tab className={classes.tabsTextProperty} label='UNHRC' />
+                  <Tab className={classes.tabsTextProperty} label='DISEC' />
+                  <Tab className={classes.tabsTextProperty} label='LokSabha' />
+                  <Tab className={classes.tabsTextProperty} label='CSW' />
+                  <Tab className={classes.tabsTextProperty} label='IMF' />
+                  <Tab className={classes.tabsTextProperty} label='CSTD' />
+                  <Tab className={classes.tabsTextProperty} label='NSG' />
+                  <Tab className={classes.tabsTextProperty} label='SPECPOL' />
+                  <Tab className={classes.tabsTextProperty} label='WHA' />
+                </Tabs>
+              </Grid>
+              <div className={classes.cardStyle}>
+                <div>
+                  <span class='flag-icon flag-icon-gr w-32'></span>
+                </div>
+                <Grid container justify='center' alignContent='space-around'>
+                  {Committe[value].map((text, index) => (
+                    <Grid
+                      item
+                      className={classes.listItemStyle}
+                      xl={2}
+                      md={4}
+                      lg={3}
+                      sm={6}
+                      xs={12}
+                    >
+                      <Card className={classes.cardProperty}>
+                        <CardContent className={classes.textProperty}>
+                          {text}
+                        </CardContent>
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </Wrapper>
   )
