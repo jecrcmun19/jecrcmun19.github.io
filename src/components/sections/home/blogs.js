9512d447da4c8de9047c6ffa12037d3f3cbe2310
Keshav <ksvkabra@gmail.com>
2020-01-28 13:29:22 +0530

    Styling changes in the blog card

--- src/components/sections/home/blogs.js
+++ src/components/sections/home/blogs.js
@@ -12,30 +12,37 @@ const HomeBlogs = () => {
   ]
 
   const description = [
-    `Dr Jawahar Surisetti is a psychologist and educationist of international repute whose 1300 + speeches, seminars and workshops have been heard by lakhs of people around the globe. He is known worldwide as the Think Professor for his "Art of Thinking " and his " Think for India " movement for introduction of thinking in education. He has received innumerable accolades for his pioneering works and thoughts on various issues of strategic interest to corporates and school as well as higher education. He advises States and central governments in India and U.S. He is also the brand ambassador of skill development for the State of Queensland. `,
-    `Dr Jawahar Surisetti is a psychologist and educationist of international repute whose 1300 + speeches, seminars and workshops have been heard by lakhs of people around the globe. He is known worldwide as the Think Professor for his "Art of Thinking " and his " Think for India " movement for introduction of thinking in education. He has received innumerable accolades for his pioneering works and thoughts on various issues of strategic interest to corporates and school as well as higher education. He advises States and central governments in India and U.S. He is also the brand ambassador of skill development for the State of Queensland. `,
-    `Dr Jawahar Surisetti is a psychologist and educationist of international repute whose 1300 + speeches, seminars and workshops have been heard by lakhs of people around the globe. He is known worldwide as the Think Professor for his "Art of Thinking " and his " Think for India " movement for introduction of thinking in education. He has received innumerable accolades for his pioneering works and thoughts on various issues of strategic interest to corporates and school as well as higher education. He advises States and central governments in India and U.S. He is also the brand ambassador of skill development for the State of Queensland. `,
+    `Dr Jawahar Surisetti is a psychologist and educationist of international repute whose 1300 + speeches, seminars and workshops have been heard by lakhs of people around the globe. He is known worldwide as the. `,
+    `Dr Jawahar Surisetti is a psychologist and educationist of international repute whose 1300 + speeches, seminars and workshops have been heard by lakhs of people around the globe. He is known worldwide as the. `,
+    `Dr Jawahar Surisetti is a psychologist and educationist of international repute whose 1300 + speeches, seminars and workshops have been heard by lakhs of people around the globe. He is known worldwide as the. `,
   ]
 
   return (
-    <div className='flex flex-wrap justify-center mx-2'>
-      <Typography className='w-full text-white font-black py-10' variant='h4'>
+    <div className='flex flex-wrap justify-center sm:px-4 px-2'>
+      <Typography
+        className='w-full text-white font-black py-10 xl:w-10/12'
+        variant='h4'
+      >
         BLOGS
       </Typography>
       {guests.map((guest, index) => (
         <div
           container
-          className='xl:w-3/12 lg:w-4/12 md:w-8/12 w-full md:p-4 py-4 h-full'
+          className='xl:w-3/12 lg:w-4/12 md:w-7/12 sm:w-10/12 h-full py-10 sm:px-4 xl:px-10'
         >
           <div className='w-full'>
             <img
               alt={guest}
               src={`/images/${guest}.jpg`}
-              className='w-full mx-auto bg-white'
+              className='mx-auto bg-white'
+              style={{
+                width: '100%',
+                height: '300px',
+              }}
             />
           </div>
 
-          <div className='p-8  bg-white'>
+          <div className='sm:p-8 p-4  bg-white'>
             <Typography className='text-center pont-extrabold' variant='h6'>
               BLOG HEADING
             </Typography>
