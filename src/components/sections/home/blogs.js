501e9eb5117002e52968d65557d579e015489d33
Keshav <ksvkabra@gmail.com>
2020-01-30 03:06:11 +0530

    added link to the read more button

--- src/components/sections/home/blogs.js
+++ src/components/sections/home/blogs.js
@@ -1,22 +1,34 @@
 import React from 'react'
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
+import { Link } from 'gatsby'
 
 const HomeBlogs = () => {
-  const blogName = ['MUN', 'advantages', 'freshmen', 'firstMUN']
-
-  const blogTitle = [
-    'What is MUN?',
-    'What are the advantages of participating in MUN?',
-    'What the college MUN freshman must know?',
-    'How does it feel like to attend your first MUN conference?',
-  ]
-
-  const description = [
-    `Have you recently heard about MUN and are curious to know what they are? Then this blog will give you an insight into the fascinating world of MUN. MUN or Model United Nations is the model of United Nations proceedings that help you experience the environment of a UN  `,
-    `Rather than being a 2-3 day event, MUN conferences are a journey. It is a procedure of learning and evolving. It is not only about winning; but becoming better versions of ourselves. What you can get at an MUN conference can be an experience of a lifetime. It has preparations, debating,`,
-    `Model United Nations in college can be challenging for the freshmen. Whether it your first MUN conference or you have achieved a lot in high school MUNs, the experience is different for all. Here are some tips for you to excel at college MUN and earn much more than just experience`,
-    `Model United Nations need a lot of pre-preparations if you want to succeed at it. Before your first MUN, anxiety and excitement are at an all time high. The pressure of performance is there. But there is so much for you to look forward to. Meet new people and have interesting conversations`,
+  const blogData = [
+    {
+      blogTitle: 'What is MUN?',
+      description: `Have you recently heard about MUN and are curious to know what they are? Then this blog will give you an insight into the fascinating world of MUN. MUN or Model United Nations is the model of United Nations proceedings that help you experience the environment of a UN  `,
+      btnLink: '/blogs/MUN',
+      image: 'MUN.jpg',
+    },
+    {
+      blogTitle: 'What are the advantages of participating in MUN?',
+      description: `Rather than being a 2-3 day event, MUN conferences are a journey. It is a procedure of learning and evolving. It is not only about winning; but becoming better versions of ourselves. What you can get at an MUN conference can be an experience of a lifetime. It has preparations, debating,`,
+      btnLink: '/blogs/advantages',
+      image: 'advantages.jpg',
+    },
+    {
+      blogTitle: 'What the college MUN freshman must know?',
+      description: `Model United Nations in college can be challenging for the freshmen. Whether it your first MUN conference or you have achieved a lot in high school MUNs, the experience is different for all. Here are some tips for you to excel at college MUN and earn much more than just experience`,
+      btnLink: '/blogs/freshmen',
+      image: 'freshmen.jpg',
+    },
+    {
+      blogTitle: 'How does it feel like to attend your first MUN conference?',
+      description: `Model United Nations need a lot of pre-preparations if you want to succeed at it. Before your first MUN, anxiety and excitement are at an all time high. The pressure of performance is there. But there is so much for you to look forward to. Meet new people and have interesting conversations`,
+      btnLink: '/blogs/firstMUN',
+      image: 'firstMUN.jpg',
+    },
   ]
 
   return (
@@ -27,12 +39,12 @@ const HomeBlogs = () => {
       >
         BLOGS
       </Typography>
-      {blogName.map((blog, index) => (
+      {blogData.map((blog, index) => (
         <div className='h-full py-10 sm:px-4' style={{ width: '300px' }}>
           <div className='w-full'>
             <img
-              alt={blog}
-              src={`/images/${blog}.jpg`}
+              alt={blog.blogTitle}
+              src={`/images/${blog.image}`}
               className='mx-auto bg-white'
               style={{
                 width: '100%',
@@ -52,7 +64,7 @@ const HomeBlogs = () => {
                 alignItems: 'center',
               }}
             >
-              {blogTitle[index]}
+              {blog.blogTitle}
             </Typography>
             <Typography
               className=' text-left text-justify pt-4'
@@ -61,14 +73,14 @@ const HomeBlogs = () => {
                 fontSize: '18px',
               }}
             >
-              {description[index]}
+              {blog.description}
             </Typography>
           </div>
           <div className='bg-white pt-4 pb-8'>
             <Button
               variant='contained'
-              // component={Link}
-              // to={section.btnLink}
+              component={Link}
+              to={blog.btnLink}
               style={{
                 background: '#d90845',
                 color: 'white',
