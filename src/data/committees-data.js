161f7f181834083c0db1cc0e1dc2410f5d3dd2ee
Rajani169 <sharmarajani169@gmail.com>
2020-01-30 15:32:28 +0530

    feat: Modify Committees Page (#70)
    * feat: Modify Committees Page
    
    * Modified Committees Data
    
    Co-authored-by: Amrit Srivastava <srivastava.amrit1999@gmail.com>

--- src/data/committees-data.js
+++ src/data/committees-data.js
@@ -5,13 +5,13 @@ export const UNODC =
   'To create a world safe from drugs and large scale organized crimes like corruption, terrorism, etc. the UNODC was established two decades ago. For large-scale threats that states can’t handle alone, UNODC offers assistance and action that is just and practical.'
 
 export const DISEC =
-  'The DISEC is a UN committee that emphasizes on global matters of war and peace, the arms and ammunitions possessed by various countries, and the effective measures to avoid international disruption of security. With varied agendas focusing on security and border policies of various nations, this is the most south-after committee at JECRC MUN for past editions.'
+   'The DISEC is a UN committee that emphasizes on global matters of war and peace, the arms and ammunitions possessed by various countries, and the effective measures to avoid international disruption of security.With varied agendas focusing on security and border policies of various nations, this is the most sought-after committee at JECRC MUN for past editions.Over the years, this committee has deliberated major issues concerning the international borders.Terrorism has assumed prime focus among the many themes, and discussions have been carried out liberally bringing the pros and cons to the forefront. The delegates representing various countries argue the sanity of their policies while simultaneously fitting their ideologies into the worldview of the issues.The Chairperson and Vice- Chairperson of the committee ensure that the discussions are agenda-centric and do not breach the code of conduct of the United Nations General Assembly. They shoulder the responsibility of arriving at relevant conclusions such that all the participating nations have been voiced and heard during the debate.At JECRC MUN, DISEC has been functioning greatly well with enthusiastic participants willing to put the stances of the nations they are representing very boldly.Witnessing DISEC unveil the layers of the agenda is a treat for the young debaters and diplomats.You now have the opportunity to be a part of this force in JECRC MUN 2020!'
 
 export const UNCSW =
-  'In the wake of progress around the globe, it is necessary to ensure that all citizens march at the same pace. The UN-CSW committee is the one that promotes gender equality and checks on women empowerment. In countries where traditions promote discrimination, this committee takes care of women’s rights.'
+  'In the wake of progress around the globe, it is necessary to ensure that all citizens march at the same pace. The UN-CSW committee is the one that promotes gender equality and checks on women empowerment. In countries where traditions promote discrimination, this committee takes care of women’s rights. Traditionally it was established to put an end to the prevalent discrimination in many nations in the name of religious laws. But, with as the time progressed, the agendas of CSW became more broad and focused also on developing ecosystems that not only gives equal rights to both genders, but also offers them opportunities to exploit their rights equally. The simulation of CSW committee at JECRC MUN has so far explored the challenging notions of role of men and boys in gender equality, the need for women empowerment, and many such. This edition’s agenda aims at establishing checks and measures against the allowances being made for women, so that the rules don’t become unfair to the male gender. Also, the main aim is not bringing women forward, rather having them come at par with men. This is to be achieved not by force or farce, but with the help of fair resources and infrastructures. JECRC MUN 2020 is simulating the ideal committee for feminists and gender equality volunteers who can get a stage to talk about the change they wish to see.'
 
 export const WHO =
-  'The WHO is an agency of United Nations entrusted with the responsibility to maintain international public health. It monitors the standards of health and living as per the global population and directs programs to help nations strengthen their healthcare services.'
+  'The WHO is an agency of United Nations entrusted with the responsibility to maintain international public health. It monitors the standards of health and living as per the global population and directs programs to help nations strengthen their healthcare services. Many countries have not been able to support its citizens and offer healthcare as one of the basic amenities of life. WHO attempts to bridge this gap between the developed nations and third-world countries! Amidst the current crisis the world is facing with outbreaks of several communicable diseases in various parts of world, the WHO has to work with more conviction. Also, one part of the world is under the impact of severe malnutrition and under-nourishment of its population, while another is struggling with obesity. All these uneven conditions make the work of WHO, all the more challenging. Medication, population control, access to safe living conditions, and pollution are some of the prime concerns of this face of United Nations at the moment. However, this is the beginning of simulation of World Health Organization as a committee at JECRC MUN. In current scenario, it is impossible to ignore the health repercussions and how a country chooses to ensure the well-being of its residents. Also, since globalization is trending, it is the moral duty of any country to help another wage a war against the diseases crippling the population. Thus, the agenda of WHO deals with the crisis world faced during the outbreak of Ebola virus in Africa.'
 
 export const AIPPM =
   'This committee simulates a platform that brings together personalities of various political parties and discuss matters for national importance. Unrestricted political debate is carried out without interference of legislative rules. It is a committee involving consensus of the members that participate in discussions and come to conclusions.'
@@ -34,10 +34,19 @@ export const Committe = {
 }
 
 export const CommitteName = {
-  0: 'UNSC',
-  1: 'UNODC',
-  2: 'DISEC',
-  3: 'UNCSW',
-  4: 'WHO',
-  5: 'AIPPM',
+  0: 'UNITED NATIONS SECURITY COUNSIL',
+  1: 'UNITED NATIONS OFFICE ON DRUGS AND CRIME',
+  2: 'DISARAMENT AND INTERNATIONAL SECURITY COMMITTEE',
+  3: 'UNITED NATIONS COMMISSION ON STATUS OF WOMEN',
+  4: 'WORLD HEALTH ORGANIZATION',
+  5: 'ALL INDIA POLITICAL PARTIES MEET',
+}
+
+export const CommitteLogo = {
+  0: 'images/committee-01.png',
+  1: 'images/committee-02.png',
+  2: 'images/committee-02.png',
+  3: 'images/committee-04.png',
+  4: 'images/committee-05.png',
+  5: 'images/committee-06.png',
 }
