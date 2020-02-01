78a86e3f7cdc6c48de208f5d0f229d11739983ba
Keshav <ksvkabra@gmail.com>
2020-02-01 19:10:15 +0530

    Added chief guests - Mani Shankar and Anand. Fix styling chief guests. Fix styling in mobile view less then 400px..

--- src/pages/index.js
+++ src/pages/index.js
@@ -76,7 +76,7 @@ export default () => {
           height='auto'
           minHeight={true}
         >
-          <div className='mx-4 pb-10'>
+          <div className='pb-10'>
             <PreviousYearChiefGuest />
           </div>
         </Banner>
@@ -93,7 +93,7 @@ export default () => {
           height='auto'
           minHeight={true}
         >
-          <div className='mx-4 pb-10'>
+          <div className='pb-10'>
             <HomeBlogs />
           </div>
         </Banner>
