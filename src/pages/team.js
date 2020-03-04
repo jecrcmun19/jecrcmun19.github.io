97eff066656324acc4680cf4ba4c88ef2e15d33f
Keshav <ksvkabra@gmail.com>
2020-03-04 11:54:46 +0530

    styling changes and team data update

--- src/pages/team.js
+++ src/pages/team.js
@@ -10,12 +10,12 @@ import makeStyles from '@material-ui/styles/makeStyles'
 import Avatar from '@material-ui/core/Avatar'
 import InstagramIcon from '@material-ui/icons/Instagram'
 import TwitterIcon from '@material-ui/icons/Twitter'
+import teams from '../data/teamDetails'
 
 const useStyles = makeStyles(theme => ({
   munTextProperty: {
     color: '#D90845',
     fontWeight: 'bold',
-    // fontFamily: "'Rubik' , sans-serif",
     fontSize: 60,
     lineHeight: '71px',
   },
@@ -23,35 +23,54 @@ const useStyles = makeStyles(theme => ({
     margin: theme.spacing(4),
     display: 'flex',
     flexWrap: 'wrap',
-    width: '80%',
+    width: '90%',
     justifyContent: 'center',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+
     [theme.breakpoints.up('sm')]: {
-      marginLeft: theme.spacing(8),
-      marginRight: theme.spacing(8),
+      width: '100%',
+    },
+    [theme.breakpoints.down('xs')]: {
+      width: '95%',
+      marginLeft: '0',
+      marginRight: '0',
+    },
+    [theme.breakpoints.up('md')]: {
+      width: '80%',
     },
     [theme.breakpoints.up('lg')]: {
+      width: '90%',
+    },
+    [theme.breakpoints.up('xl')]: {
       width: '70%',
     },
-    marginLeft: theme.spacing(0),
-    marginRight: theme.spacing(0),
   },
   container: {
     display: 'flex',
     position: 'relative',
-    width: '400px',
-    height: '400px',
-    maxHeight: '400px',
+    width: '350px',
+    height: '350px',
+    maxHeight: '350px',
     color: '#FFF',
-    margin: theme.spacing(4),
+    margin: theme.spacing('4'),
+    [theme.breakpoints.down('xs')]: {
+      marginLeft: '5px',
+      marginRight: '5px',
+    },
+    [theme.breakpoints.down('md')]: {
+      marginLeft: theme.spacing(2),
+      marginRight: theme.spacing(2),
+    },
     '&:hover > img': {
       filter: 'brightness(0.5)',
     },
     '&:hover > div': {
-      visibility: 'hidden',
+      opacity: '0',
       transition: 'none',
     },
     '&:hover > main': {
-      visibility: 'visible',
+      opacity: '1',
       paddingBottom: theme.spacing(4),
     },
   },
@@ -64,13 +83,13 @@ const useStyles = makeStyles(theme => ({
     animation: '$cssAnimation 0s 3s ease-out',
     textAlign: 'center',
     bottom: 0,
-    visibility: 'visible',
-    clipPath: 'polygon(100% 100%, 0% 100%, 50% -95%)',
+    opacity: '1',
+    clipPath: 'polygon(100% 100%, 0% 100%, 50% -180%)',
     background: '#d90845c4',
   },
   imageStyle: {
     width: '100%',
-    height: '400px',
+    height: '350px',
   },
   hoverContainer: {
     display: 'flex',
@@ -80,7 +99,7 @@ const useStyles = makeStyles(theme => ({
     position: 'absolute',
     width: '100%',
     height: '100%',
-    visibility: 'hidden',
+    opacity: '0',
     transition: 'all 0.3s ease-out',
     textAlign: 'center',
     bottom: 0,
@@ -101,7 +120,6 @@ const useStyles = makeStyles(theme => ({
   },
   hoverText: {
     bottom: 0,
-    // paddingBottom: theme.spacing(2),
   },
 }))
 
@@ -149,49 +167,70 @@ function Team(props) {
         fluid={bgImage.sharp.fluid}
         durationFadeIn={50}
       >
-        <div className={classes.root}>
-          {['1', '2', '3', '4', '5', '6'].map((value, index) => (
-            <div key={index} className={classes.container}>
-              <img
-                className={classes.imageStyle}
-                src='images/anandChulani.jpg'
-              />
-              <Grid className={classes.bottomText}>
-                <Typography variant='h6'>John Doe</Typography>
-                <Typography variant='subtitle1'>Developer</Typography>
-              </Grid>
-              <Grid className={classes.hoverContainer} component='main'>
-                <Grid
-                  component='p'
-                  container
-                  justify='center'
-                  className={classes.socialIcons}
+        {teams.map((value, index) => (
+          <div className='w-full flex justify-center'>
+            <div className={classes.root}>
+              <div className='w-full'>
+                <Typography
+                  className='pt-10 text-black text-center'
+                  variant='h4'
                 >
-                  <Avatar
-                    component='a'
-                    href='https://instagram.com/jecrcmun'
-                    target='_blank'
-                    className={['m-2', classes.socialIcon]}
-                  >
-                    <InstagramIcon className={classes.icon} />
-                  </Avatar>
-                  <Avatar
-                    component='a'
-                    href='https://twitter.com/jecrcmun'
-                    target='_blank'
-                    className={['m-2', classes.socialIcon]}
-                  >
-                    <TwitterIcon className={classes.icon} />
-                  </Avatar>
-                </Grid>
-                <Grid className={classes.hoverText}>
-                  <Typography variant='h5'>John Doe</Typography>
-                  <Typography variant='subtitle1'>Developer</Typography>
-                </Grid>
-              </Grid>
+                  {value.teamName}
+                </Typography>
+                <img
+                  src='/images/line.png'
+                  className='mx-auto pb-10 '
+                  alt='---------------------'
+                />
+              </div>
+              {value.members.map((value, index) => (
+                <div key={index} className={classes.container}>
+                  <img
+                    className={classes.imageStyle}
+                    src='images/anandChulani.jpg'
+                  />
+                  <Grid className={classes.bottomText}>
+                    <Typography variant='h6'>{value.name}</Typography>
+                    <Typography variant='subtitle1'>
+                      {value.designation}
+                    </Typography>
+                  </Grid>
+                  <Grid className={classes.hoverContainer} component='main'>
+                    <Grid
+                      component='p'
+                      container
+                      justify='center'
+                      className={classes.socialIcons}
+                    >
+                      <Avatar
+                        component='a'
+                        href='https://instagram.com/jecrcmun'
+                        target='_blank'
+                        className={['m-2', classes.socialIcon]}
+                      >
+                        <InstagramIcon className={classes.icon} />
+                      </Avatar>
+                      <Avatar
+                        component='a'
+                        href='https://twitter.com/jecrcmun'
+                        target='_blank'
+                        className={['m-2', classes.socialIcon]}
+                      >
+                        <TwitterIcon className={classes.icon} />
+                      </Avatar>
+                    </Grid>
+                    <Grid className={classes.hoverText}>
+                      <Typography variant='h5'>{value.name}</Typography>
+                      <Typography variant='subtitle1'>
+                        {value.designation}
+                      </Typography>
+                    </Grid>
+                  </Grid>
+                </div>
+              ))}
             </div>
-          ))}
-        </div>
+          </div>
+        ))}
       </BackgroundImage>
     </Wrapper>
   )
